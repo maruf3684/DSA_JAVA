1ed9ab0680ef7241addeab760a0da8c81a16cff4
“maruf3684” <maruf3684@gmail.com>
2023-08-20 13:18:07 +0600

    new changes

--- src/Searching/Binar.java
+++ src/Searching/Binar.java
@@ -1,6 +1,6 @@
 package Searching;
 
-public class Binary {
+public class Binar {
     public static void main(String[] args) {
         int[] arr = {3,4,5,6,7,9,12,16,17,100,233,566,777};
         int find=566;
