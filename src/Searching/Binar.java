8e9900f183e6cd15a39a32d62acf823fa0fb8258
“maruf3684” <maruf3684@gmail.com>
2023-08-20 13:17:37 +0600

    changes

---
+++ src/Searching/Binar.java
@@ -0,0 +1,28 @@
+package Searching;
+
+public class Binary {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,6,7,9,12,16,17,100,233,566,777};
+        int find=566;
+        System.out.println(binary(arr,find));
+    }
+    public static int binary(int[] arr,int find){
+        int start=0;
+        int end= arr.length-1;
+        int mid = (start+end)/2;
+
+        while (start<=end){
+            if (arr[mid] < find){
+                start=mid+1;
+                mid = (start+end)/2;
+            }else if (arr[mid] > find){
+                end=mid-1;
+                mid = (start+end)/2;
+            }else {
+                return arr[mid];
+            }
+        }
+
+        return -1;
+    }
+}
