6e943d6f981d29b0113848fcd49421afb6ed7903
“maruf3684” <maruf3684@gmail.com>
2023-08-25 13:25:56 +0600

    2

---
+++ src/Arry/IntersectionArrays.java
@@ -0,0 +1,25 @@
+package Arry;
+
+import java.util.ArrayList;
+
+public class IntersectionArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1,1,2,3,4,5};
+        int[] arr2 = {2,3,4,4,5,6};
+        ArrayList<Integer> unionArr= new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i<arr1.length && j< arr2.length){
+            if(arr1[i]<arr2[j]){
+                i=i+1;
+            } else if (arr1[i]>arr2[j]) {
+                j=j+1;
+            }else {
+                unionArr.add(arr1[i]);
+                i=i+1;
+                j=j+1;
+            }
+        }
+        System.out.println(unionArr);
+    }
+}
---
+++ src/Arry/RightRotate.java
@@ -0,0 +1,32 @@
+package Arry;
+
+import java.util.Arrays;
+
+public class RightRotate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(arr));
+        int k=3;
+        int kk =k % arr.length;
+        int[] arr2 = new int[kk];
+
+        int i = arr.length-kk;
+        int j =0;
+        while (i<arr.length){
+            arr2[j] = arr[i];
+            i=i+1;
+            j=j+1;
+        }
+
+        for (int l = arr.length-1; l >= kk; l--) {
+            arr[l]= arr[l-kk];
+        }
+
+        for (int m = 0; m < arr2.length; m++) {
+            arr[m]=arr2[m];
+        }
+
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr));
+    }
+}
