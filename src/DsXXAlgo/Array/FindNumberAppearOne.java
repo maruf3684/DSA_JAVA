548c8b2b4db11ca8518aeb8d79411a5592284a73
maruf <maruf3684@gmail.com>
2024-03-22 02:37:37 +0600

    new array 2

---
+++ src/DsXXAlgo/Array/FindNumberAppearOne.java
@@ -0,0 +1,13 @@
+package DsXXAlgo.Array;
+
+public class FindNumberAppearOne {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,2,3,4};
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans = ans^nums[i];
+        }
+
+        System.out.println(ans);
+    }
+}
