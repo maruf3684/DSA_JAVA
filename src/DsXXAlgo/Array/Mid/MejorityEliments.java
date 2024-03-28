ca5cf4f8670c46dd6239c7985132b6f52e2b3054
maruf <maruf3684@gmail.com>
2024-03-29 03:46:24 +0600

    new array 7

--- src/DsXXAlgo/Array/Mid/MejorityEliments.java
+++ src/DsXXAlgo/Array/Mid/MejorityEliments.java
@@ -1,5 +1,7 @@
 package DsXXAlgo.Array.Mid;
 
+//Given an array nums of size n, return the majority element.
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 public class MejorityEliments {
     public static void main(String[] args) {
         int[] nums = {2,2,1,1   ,1,2,2};
