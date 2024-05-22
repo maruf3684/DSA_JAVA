e6c68f278d61d23c96433a7057f0e7b718bbe2d1
maruf <maruf3684@gmail.com>
2024-05-22 16:36:06 +0600

    new array 8

---
+++ src/DsXXAlgo/DP/ClimbingStars.java
@@ -0,0 +1,47 @@
+package DsXXAlgo.DP;
+
+public class ClimbingStars {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(dp(n));
+        System.out.println(dp2(n));
+    }
+
+    public static int dp(int n){
+        if(n<0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        int option1 = dp(n-1);
+        int option2 = dp(n-2);
+        return option1+option2;
+    }
+
+    public static int dp2(int n){
+        int onelaf = 0;
+        int tholaf = 0;
+        int way = 0;
+
+        if(n==1){
+            return 1;
+        }else if(n==2){
+            return 2;
+        }
+
+        for (int i = 0; i <= n; i++) {
+            if(i==1){
+                onelaf = 1;
+            }else if(i==2){
+                tholaf = 2;
+            }else {
+                way = onelaf+tholaf;
+                onelaf = tholaf;
+                tholaf = way;
+            }
+        }
+
+        return way;
+    }
+}
---
+++ src/DsXXAlgo/Patern/Wpatern.java
@@ -0,0 +1,50 @@
+package DsXXAlgo.Patern;
+
+import java.util.Arrays;
+
+public class Wpatern {
+    public static void main(String[] args) {
+        String st = "ABCDEFGHIJKLMNOPQ";
+        int frequency = 5;
+
+        //solution
+        int m = frequency;
+        int n = st.length();
+        char[][] arr = new char[m][n];
+
+        int printer = 0;
+        boolean reverse = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(j == printer){
+                    arr[j][i] = st.charAt(i);
+                    if(reverse == false){
+                        printer = printer+1;
+                    }else {
+                        printer = printer-1;
+                    }
+
+                    if(printer == frequency-1){
+                        reverse = true;
+                    } else if (printer==0) {
+                        reverse = false;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(arr[i][j] == '\u0000' ){
+                    System.out.print(" ");
+                }else {
+                    System.out.print(arr[i][j]);
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
