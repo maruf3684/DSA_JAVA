a9d401a26dde6bb28d92d9b09923314cbfd083c1
maruf <maruf3684@gmail.com>
2024-09-12 07:50:23 +0600

    sliding w

--- src/DsXXAlgo/SlidingWindow/MinimumWindowSubstring.java
+++ src/DsXXAlgo/SlidingWindow/MinimumWindowSubstring.java
@@ -1,67 +1,55 @@
 package DsXXAlgo.SlidingWindow;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class MinimumWindowSubstring {
-    public  static Map<Character,Integer> s1Map= new HashMap<>();
-    public  static Map<Character,Integer> s2Map= new HashMap<>();
     public static void main(String[] args) {
-        System.out.println(minWindow("ADOBECODEBANC","ABC"));
+        System.out.println(minWindow("aab","aab"));
     }
-
     public static String minWindow(String s2, String s1) {
-        int [] getarr = minWindowfinder( s2, s1);
-        int fast;
-        String substring;
-        if (getarr.length != 0){
-            fast = getarr[0];
-            substring = s2.substring(0,fast+1);
-            for (int i = 0; i < substring.length(); i++) {
-                Character c = s2.charAt(i);
-                s2Map.put(c,s2Map.get(c)-1);
-                if(check()){
-                    substring = substring.substring(i+1);
-                }
-            }
-            return substring;
-        }
-        return "-";
-    }
-    public static int[] minWindowfinder(String s2, String s1) {
+        if(s2.length()<s1.length()) return "";
+        Map<Character,Integer> s1map = new HashMap<>();
+        Map<Character,Integer> s2map = new HashMap<>();
+
         for (int i = 0; i < s1.length(); i++) {
             Character ch = s1.charAt(i);
-            s1Map.put(ch, s1Map.getOrDefault(ch, 0) == 0 ? 1 : s1Map.get(ch)+1);
+            s1map.put(ch, 1+s1map.getOrDefault(ch,0));
+            s2map.put(ch,0);
         }
 
         int fast = 0;
         int slow = 0;
-        boolean find;
+        int[] length = new int[]{0,s2.length()+1};
+        int have = 0;
 
         while (fast<s2.length()){
             Character ch = s2.charAt(fast);
-            s2Map.put(ch, s2Map.getOrDefault(ch, 0) == 0 ? 1 : s2Map.get(ch)+1);
-
-            int windowLengh = (fast-slow) + 1;
-            if(windowLengh >= s1.length()){
-               find = check();
-               if (find){
-                   return new int[]{fast};
-               }
+            if(s2map.containsKey(ch)){
+                s2map.put(ch, 1+s2map.getOrDefault(ch,0));
+                if (Objects.equals(s2map.get(ch), s1map.get(ch))){
+                    have ++;
+                }
+            }
+            while (have==s2map.size()){
+                if((fast-slow)+1 < (length[1]-length[0])+1){
+                    length[0] = slow;
+                    length[1] = fast;
+                }
+                Character slowCh = s2.charAt(slow);
+                if(s2map.containsKey(slowCh)){
+                    s2map.put(slowCh,s2map.get(slowCh)-1);
+                    if (s2map.get(slowCh)<s1map.get(slowCh)){
+                        have --;
+                    }
+                }
+                slow ++;
             }
             fast++;
         }
-        return new int[0];
-    }
 
-    public  static  boolean check(){
-        boolean isAvailable = true;
-        for (Character key : s1Map.keySet()) {
-            if(!s1Map.get(key).equals(s2Map.get(key))){
-                isAvailable = false;
-                break;
-            }
-        }
-        return isAvailable;
+        return length[1]==s2.length()+1 ?"": s2.substring(length[0],length[1]+1);
     }
 }
